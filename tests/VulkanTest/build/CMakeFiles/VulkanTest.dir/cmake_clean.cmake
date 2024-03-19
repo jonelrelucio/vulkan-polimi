4eb8d5cd48a2674824d05639b23d7122ba26d2f5
jonelrelucio <reluciojonel@gmail.com>
2024-03-19 20:34:29 +0100

    Vulkan Installation

---
+++ tests/VulkanTest/build/CMakeFiles/VulkanTest.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/VulkanTest.dir/main.cpp.o"
+  "CMakeFiles/VulkanTest.dir/main.cpp.o.d"
+  "VulkanTest"
+  "VulkanTest.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/VulkanTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
